3c24c9af6ac7e9b300b126021cbb5bff575e6a60
Alan Marvel <alan@mrvl.io>
2019-08-01 21:58:08 -0400

    Add rand

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int
 main()
 {
-	printf("Hello world\n");
+	srand(time(NULL));
+	int die = 6;
+	int roll = rand() % die + 1;
+	int result = 1 * (roll) + 6;
+
+	printf("1D%d+6=%d\n", die, result);
+
 	return 0;
 }
